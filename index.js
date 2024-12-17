1111e58e0a340c8920fb1388d8db043829407d38
VINAY KUMAR <vinaykumar.officaldev@gmail.com>
2024-12-17 23:14:48 +0530

    add initial files for node-pty and nan dependencies

--- index.js
+++ index.js
@@ -1,15 +1,86 @@
 const http = require("http");
 const express = require("express");
-const { Socket: socketServer } = require("socket.io");
+const fs = require("fs/promises");
+const { Server: SocketServer } = require("socket.io");
+const path = require("path");
+const cors = require("cors");
+const chokidar = require("chokidar");
+
+const pty = require("node-pty");
+
+const ptyProcess = pty.spawn("bash", [], {
+  name: "xterm-color",
+  cols: 80,
+  rows: 30,
+  cwd: process.env.INIT_CWD + "/user",
+  env: process.env,
+});
 
 const app = express();
 const server = http.createServer(app);
-const io = new socketServer({
+const io = new SocketServer({
   cors: "*",
 });
 
+app.use(cors());
+
 io.attach(server);
 
-io.on("connection", (socket) => console.log("socket connected", socket.id));
+chokidar.watch("./user").on("all", (event, path) => {
+  io.emit("file:refresh", path);
+});
 
-server.listen(8000, `socket server connected at port 8000`);
+ptyProcess.onData((data) => {
+  io.emit("terminal:data", data);
+});
+
+io.on("connection", (socket) => {
+  console.log(`Socket connected`, socket.id);
+
+  socket.emit("file:refresh");
+
+  socket.on("file:change", async ({ path, content }) => {
+    await fs.writeFile(`./user${path}`, content);
+  });
+
+  socket.on("terminal:write", (data) => {
+    console.log("Term", data);
+    ptyProcess.write(data);
+  });
+});
+
+app.get("/files", async (req, res) => {
+  const fileTree = await generateFileTree("./user");
+  return res.json({ tree: fileTree });
+});
+
+app.get("/files/content", async (req, res) => {
+  const path = req.query.path;
+  const content = await fs.readFile(`./user${path}`, "utf-8");
+  return res.json({ content });
+});
+
+server.listen(9000, () => console.log(`🐳 Docker server running on port 9000`));
+
+async function generateFileTree(directory) {
+  const tree = {};
+
+  async function buildTree(currentDir, currentTree) {
+    const files = await fs.readdir(currentDir);
+
+    for (const file of files) {
+      const filePath = path.join(currentDir, file);
+      const stat = await fs.stat(filePath);
+
+      if (stat.isDirectory()) {
+        currentTree[file] = {};
+        await buildTree(filePath, currentTree[file]);
+      } else {
+        currentTree[file] = null;
+      }
+    }
+  }
+
+  await buildTree(directory, tree);
+  return tree;
+}
